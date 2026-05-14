019a5969ab69ebded018042c47b67b27b400799c
HonorineByela <ablysedate@gmail.com>
2026-05-14 17:14:56 +0300

    Update server.js

--- server.js
+++ server.js
@@ -1,16 +1,42 @@
 require("dotenv").config();
 
 const express = require("express");
+const helmet = require("helmet");
+const rateLimit = require("express-rate-limit");
 const path = require("path");
 
 const app = express();
 
-// Serve static files
+/* SECURITY */
+
+app.use(helmet());
+
+app.use(
+  rateLimit({
+    windowMs: 15 * 60 * 1000,
+    max: 100,
+  })
+);
+
+/* BODY PARSER */
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+/* STATIC FILES */
+
 app.use(express.static(__dirname));
 
-// Homepage
+/* HOME PAGE */
+
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "index.html"));
 });
 
-module.exports = app;
+/* SERVER */
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
